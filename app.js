bec93b909233b9cea1ddb85d2545dd04e0e0075a
Your Name <you@example.com>
2025-01-20 13:16:08 -0300

    create db connection

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const app = express();
+const connection = require("./db/connection");
+connection();
 //Public Route
 app.get("/", (req, res) => {
     res.status(200).json({
         msg: "Hello Hero"
     })
+});
+app.post("auth/register", (req, res) => {
+    const {name, email, pass, confirmPass} = req.body;
 });
 app.listen(3000);
